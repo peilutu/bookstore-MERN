c9d279dbf8058b76bdd6dc4571bbe5902e425034
peilutu <85421108+peilutu@users.noreply.github.com>
2023-11-07 17:21:43 -0800

    add book model

--- frontend/src/components/home/BooksCard.jsx
+++ frontend/src/components/home/BooksCard.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BsInfoCircle } from "react-icons/bs";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BiUserCircle } from "react-icons/bi";
+import { PiBookOpenTextLight } from "react-icons/pi";
+import { MdOutlineDelete } from "react-icons/md";
+import { Link } from "react-router-dom";
+import BookSinglecCard from "./BookSinglecCard";
 
-const BooksCard = () => {
-  return <div>BooksCard</div>;
+const BooksCard = ({ books }) => {
+  console.log(books);
+  return (
+    <div className="grid sm:gird-cols-2 lg:grid-cols-3 xl:grid-cols-4 ">
+      {books.map((book) => (
+        <BookSinglecCard key={book._id} book={book} />
+      ))}
+    </div>
+  );
 };
 
 export default BooksCard;
